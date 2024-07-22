91de1b9f9809995ba77d3458775d769794c6dcfa
harshxraj <harshraj0588@gmail.com>
2024-07-22 14:15:03 +0530

    all animations done

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -2,7 +2,6 @@
 import ArrowRight from "@/assets/arrow-right.svg";
 import starImage from "@/assets/star.png";
 import springImage from "@/assets/spring.png";
-import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 
