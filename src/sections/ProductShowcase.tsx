1d651d3db7c5440c0ff61fb70e93ea54d8b62090
harshxraj <harshraj0588@gmail.com>
2024-07-22 13:29:14 +0530

    Product Showcase Animation  done

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,11 +1,22 @@
+"use client";
 import productImage from "@/assets/product-image.png";
 import pyramidImage from "@/assets/pyramid.png";
 import tubeImage from "@/assets/tube.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+    <section ref={sectionRef} className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
         <div className="max-w-[540px] mx-auto">
           <div className="flex justify-center">
@@ -23,19 +34,25 @@ export const ProductShowcase = () => {
 
         <div className="relative">
           <Image src={productImage} alt="Product image" className="mt-10" />
-          <Image
-            src={pyramidImage}
+          <motion.img
+            src={pyramidImage.src}
             alt="Pyramid image"
             height={262}
             width={262}
             className="hidden md:block absolute -right-36 -top-32"
+            style={{
+              translateY: translateY,
+            }}
           />
-          <Image
-            src={tubeImage}
+          <motion.img
+            src={tubeImage.src}
             alt="Tube image"
             height={248}
             width={248}
-            className="hidden md:block absolute bottom-24 -left-36 "
+            className="hidden md:block absolute bottom-24 -left-36"
+            style={{
+              translateY: translateY,
+            }}
           />
         </div>
       </div>
