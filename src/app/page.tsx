4c25128c5b934cffd5613304368d27bf2abd322b
harshxraj <harshraj0588@gmail.com>
2024-07-22 12:42:13 +0530

    Call to Action Section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { CallToAction } from "@/sections/CallToAction";
+import { Footer } from "@/sections/Footer";
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
@@ -14,6 +16,8 @@ export default function Home() {
       <ProductShowcase />
       <Pricing />
       <Testimonials />
+      <CallToAction />
+      <Footer />
     </div>
   );
 }
--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,41 @@
+import ArrowRight from "@/assets/arrow-right.svg";
+import starImage from "@/assets/star.png";
+import springImage from "@/assets/spring.png";
+import Image from "next/image";
+
 export const CallToAction = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="container">
+        <div className="section-heading relative">
+          <h2 className="section-title">Sign up for free today</h2>
+          <p className="section-des mt-5">
+            Celebrate the joy of accomplishment with an app designed to track your progress and motivate your
+            efforts.
+          </p>
+
+          <Image
+            src={starImage}
+            alt="star image"
+            width={360}
+            className="absolute -left-[350px] -top-[137px]"
+          />
+          <Image
+            src={springImage}
+            alt="spring image"
+            width={360}
+            className="absolute -right-[331px] -top-[19px]"
+          />
+        </div>
+
+        <div className="flex gap-2 mt-10 justify-center">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text gap-1">
+            <span>Learn more</span>
+            <ArrowRight className="h-5 w-5" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
